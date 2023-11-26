402319f456273fff343a0c8b87c0aa2d83abb83f
gustavodepaulasilva2 <99282594+gustavodepaulasilva2@users.noreply.github.com>
2023-11-26 10:05:17 -0300

    Add files via upload

---
+++ PRO_1-1_C116_TemplateDoProjeto-main/PRO_1-1_C116_TemplateDoProjeto-main/family tree/app.py
@@ -0,0 +1,54 @@
+# importando os módulos da biblioteca flask
+from flask import Flask , render_template
+
+# criando a instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+app = Flask(__name__)
+
+# escreva as rotas usando funções de decorador
+# rota padrão ou 'URL'
+@app.route("/")
+def home():
+
+    nome = "Gustavo" # escreva seu nome
+    idade = "14" # escreva sua idade
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página do pai
+@app.route("/pai")
+
+def pai_flask():
+    nome = "Adair" # escreva seu nome
+    idade = "54" # escreva sua idade
+
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página da mãe
+@app.route("/mãe")
+
+def mae_flask():
+
+    nome = "Rita" # escreva seu nome
+    idade = "46" # escreva sua idade
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página do amigo
+@app.route("/amigo")
+
+def amigo_flask():
+    nome = "Alex" # escreva seu nome
+    idade = "15" # escreva sua idade
+
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# adicione outras rotas, se você quiser
+
+
+
+
+# execute o arquivo
+if __name__  ==  '__main__':
+    app.run(debug=True)
